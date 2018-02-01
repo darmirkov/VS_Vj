ecf856696e60fac9a7d8a5db2230a1b99cbab279
darmirkov <32600755+darmirkov@users.noreply.github.com>
2018-02-01 03:01:20 +0100

    Add files via upload

---
+++ 5.1 Varijable/5.1.1.Zbroj_i_razlika/5.1.1.Zbroj_i_razlika/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1._1.Zbroj_i_razlika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.Write("Unesite prvi broj: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Unesite drugi broj: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zbroj je: {0}", a+b);
+            Console.WriteLine("Razlika je: {0}", a-b);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5.1 Varijable/5.1.2.Umnoz_kvoc/5.1.2.Umnoz_kvoc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1._2.Umnoz_kvoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.Write("Unesite prvi broj: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Unesite drugi broj: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Umnozak je: {0}", a*b);
+            Console.WriteLine("Kvocijent je: {0}", (float)a / (float)b);
+            Console.ReadKey();
+        }
+    }
+}
